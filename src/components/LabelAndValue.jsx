4c5d8e968663826c5f6eab1c89aae783cbd3daf5
sam-chandgaonkar <sampiti26@gmail.com>
2024-10-23 17:00:59 +0530

    sc - unstable

--- src/components/LabelAndValue.jsx
+++ src/components/LabelAndValue.jsx
@@ -1,15 +1,12 @@
 function LabelAndValue({ label, value }) {
- return (
+  return (
     <div className="output-wrapper">
-    <div className="row">
-      <div className="col-5">
-        <h5>{label || "Please add Label Prop"}</h5>
-      </div>
-      <div className="col-6">
-        <h5>{value || "Please add value Prop"}</h5>
+      <div className="row">
+        <span>
+          {label}:{value}
+        </span>
       </div>
     </div>
-  </div>
- )
+  );
 }
 export default LabelAndValue;
